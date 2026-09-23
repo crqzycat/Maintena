7a6ab2e197148b315d5282b46fb3aa01c92f3b12
crqzycat <crqzycat@gmail.com>
2026-09-23 22:27:16 +0200

    Remove unused command registration code in Maintena for clarity

--- src/main/java/crqzycat/maintena/Maintena.java
+++ src/main/java/crqzycat/maintena/Maintena.java
@@ -2,18 +2,12 @@ package crqzycat.maintena;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import crqzycat.maintena.maintenance.MaintenanceManager;
 
 public class Maintena implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        // Register commands
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            // Commands will be registered here if needed
-        });
-        
         // Initialize manager on server start
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
             MaintenanceManager.getInstance().setServer(server);
