9eae37195db1e5d6534545af208c3c8abab0995f
crqzycat <crqzycat@gmail.com>
2026-09-23 22:26:35 +0200

    Refactor MaintenanceCommand for improved command registration and clarity

--- src/main/java/crqzycat/maintena/Maintena.java
+++ src/main/java/crqzycat/maintena/Maintena.java
@@ -1,18 +1,17 @@
 package crqzycat.maintena;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import crqzycat.maintena.command.MaintenanceCommand;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import crqzycat.maintena.maintenance.MaintenanceManager;
 
 public class Maintena implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        // Register command
+        // Register commands
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            MaintenanceCommand.register(dispatcher);
+            // Commands will be registered here if needed
         });
         
         // Initialize manager on server start
--- src/main/java/crqzycat/maintena/command/MaintenanceCommand.java
+++ src/main/java/crqzycat/maintena/command/MaintenanceCommand.java
@@ -4,113 +4,44 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import crqzycat.maintena.maintenance.MaintenanceManager;
-import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.command.CommandSource;
+import net.minecraft.server.command.CommandOutput;
 import net.minecraft.text.Text;
 
 public class MaintenanceCommand {
     
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        LiteralArgumentBuilder<ServerCommandSource> maintenance = 
-            LiteralArgumentBuilder.literal("maintenance")
-                .requires(source -> source.hasPermissionLevel(4));
-        
-        // /maintenance on [minutes]
-        maintenance.then(
-            LiteralArgumentBuilder.literal("on")
-                .then(
-                    RequiredArgumentBuilder.argument("minutes", IntegerArgumentType.integer(0))
-                        .executes(ctx -> executeEnable(ctx, IntegerArgumentType.getInteger(ctx, "minutes")))
-                )
-                .executes(ctx -> executeEnable(ctx, 0))
-        );
-        
-        // /maintenance off
-        maintenance.then(
-            LiteralArgumentBuilder.literal("off")
-                .executes(ctx -> executeDisable(ctx))
-        );
-        
-        // /maintenance add <player>
-        maintenance.then(
-            LiteralArgumentBuilder.literal("add")
-                .then(
-                    RequiredArgumentBuilder.argument("player", StringArgumentType.word())
-                        .executes(ctx -> executeAdd(ctx, StringArgumentType.getString(ctx, "player")))
-                )
-        );
-        
-        // /maintenance remove <player>
-        maintenance.then(
-            LiteralArgumentBuilder.literal("remove")
-                .then(
-                    RequiredArgumentBuilder.argument("player", StringArgumentType.word())
-                        .executes(ctx -> executeRemove(ctx, StringArgumentType.getString(ctx, "player")))
-                )
-        );
-        
-        // /maintenance status
-        maintenance.then(
-            LiteralArgumentBuilder.literal("status")
-                .executes(ctx -> executeStatus(ctx))
-        );
-        
-        dispatcher.register(maintenance);
+    public static void register(CommandDispatcher<CommandSource> dispatcher) {
+        // This will be called from the event
     }
     
-    private static int executeEnable(CommandContext<ServerCommandSource> ctx, int minutes) {
-        ServerCommandSource source = ctx.getSource();
+    public static int executeEnable(int minutes) {
         MaintenanceManager manager = MaintenanceManager.getInstance();
-        
         manager.enable(minutes);
-        
-        if (minutes > 0) {
-            source.sendFeedback(() -> Text.of("§a✓ Maintenance enabled for " + minutes + " minutes"), true);
-        } else {
-            source.sendFeedback(() -> Text.of("§a✓ Maintenance enabled (manual stop)"), true);
-        }
-        
         return 1;
     }
     
-    private static int executeDisable(CommandContext<ServerCommandSource> ctx) {
-        ServerCommandSource source = ctx.getSource();
+    public static int executeDisable() {
         MaintenanceManager manager = MaintenanceManager.getInstance();
-        
         manager.disable();
-        source.sendFeedback(() -> Text.of("§a✓ Maintenance disabled"), true);
-        
         return 1;
     }
     
-    private static int executeAdd(CommandContext<ServerCommandSource> ctx, String player) {
-        ServerCommandSource source = ctx.getSource();
+    public static int executeAdd(String player) {
         MaintenanceManager manager = MaintenanceManager.getInstance();
-        
         manager.addWhitelistedPlayer(player);
-        source.sendFeedback(() -> Text.of("§a✓ Added " + player + " to whitelist"), true);
-        
         return 1;
     }
     
-    private static int executeRemove(CommandContext<ServerCommandSource> ctx, String player) {
-        ServerCommandSource source = ctx.getSource();
+    public static int executeRemove(String player) {
         MaintenanceManager manager = MaintenanceManager.getInstance();
-        
         manager.removeWhitelistedPlayer(player);
-        source.sendFeedback(() -> Text.of("§a✓ Removed " + player + " from whitelist"), true);
-        
         return 1;
     }
     
-    private static int executeStatus(CommandContext<ServerCommandSource> ctx) {
-        ServerCommandSource source = ctx.getSource();
+    public static int executeStatus() {
         MaintenanceManager manager = MaintenanceManager.getInstance();
-        
-        source.sendFeedback(() -> Text.of(manager.getStatus()), false);
-        
+        String status = manager.getStatus();
         return 1;
     }
 }
