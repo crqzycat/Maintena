4b9ecbc49624106acaa7a53578fc1048ea9d935c
crqzycat <crqzycat@gmail.com>
2026-09-23 22:25:23 +0200

    Refactor MaintenanceCommand registration for improved readability and structure

--- src/main/java/crqzycat/maintena/command/MaintenanceCommand.java
+++ src/main/java/crqzycat/maintena/command/MaintenanceCommand.java
@@ -4,30 +4,60 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import crqzycat.maintena.maintenance.MaintenanceManager;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 
 public class MaintenanceCommand {
     
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(CommandManager.literal("maintenance")
-                .requires(source -> source.hasPermissionLevel(4))
-                .then(CommandManager.literal("on")
-                        .then(CommandManager.argument("minutes", IntegerArgumentType.integer(0))
-                                .executes(ctx -> executeEnable(ctx, IntegerArgumentType.getInteger(ctx, "minutes"))))
-                        .executes(ctx -> executeEnable(ctx, 0)))
-                .then(CommandManager.literal("off")
-                        .executes(ctx -> executeDisable(ctx)))
-                .then(CommandManager.literal("add")
-                        .then(CommandManager.argument("player", StringArgumentType.word())
-                                .executes(ctx -> executeAdd(ctx, StringArgumentType.getString(ctx, "player")))))
-                .then(CommandManager.literal("remove")
-                        .then(CommandManager.argument("player", StringArgumentType.word())
-                                .executes(ctx -> executeRemove(ctx, StringArgumentType.getString(ctx, "player")))))
-                .then(CommandManager.literal("status")
-                        .executes(ctx -> executeStatus(ctx))));
+        LiteralArgumentBuilder<ServerCommandSource> maintenance = 
+            LiteralArgumentBuilder.literal("maintenance")
+                .requires(source -> source.hasPermissionLevel(4));
+        
+        // /maintenance on [minutes]
+        maintenance.then(
+            LiteralArgumentBuilder.literal("on")
+                .then(
+                    RequiredArgumentBuilder.argument("minutes", IntegerArgumentType.integer(0))
+                        .executes(ctx -> executeEnable(ctx, IntegerArgumentType.getInteger(ctx, "minutes")))
+                )
+                .executes(ctx -> executeEnable(ctx, 0))
+        );
+        
+        // /maintenance off
+        maintenance.then(
+            LiteralArgumentBuilder.literal("off")
+                .executes(ctx -> executeDisable(ctx))
+        );
+        
+        // /maintenance add <player>
+        maintenance.then(
+            LiteralArgumentBuilder.literal("add")
+                .then(
+                    RequiredArgumentBuilder.argument("player", StringArgumentType.word())
+                        .executes(ctx -> executeAdd(ctx, StringArgumentType.getString(ctx, "player")))
+                )
+        );
+        
+        // /maintenance remove <player>
+        maintenance.then(
+            LiteralArgumentBuilder.literal("remove")
+                .then(
+                    RequiredArgumentBuilder.argument("player", StringArgumentType.word())
+                        .executes(ctx -> executeRemove(ctx, StringArgumentType.getString(ctx, "player")))
+                )
+        );
+        
+        // /maintenance status
+        maintenance.then(
+            LiteralArgumentBuilder.literal("status")
+                .executes(ctx -> executeStatus(ctx))
+        );
+        
+        dispatcher.register(maintenance);
     }
     
     private static int executeEnable(CommandContext<ServerCommandSource> ctx, int minutes) {
