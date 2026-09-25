bb7aff48ab70cd3b8ee01a9ace74733962de0114
crqzycat <crqzycat@gmail.com>
2026-09-25 09:01:20 +0200

    Refactor maintenance command handling: update permission check method for maintenance commands

--- src/main/java/crqzycat/maintena/command/MaintenanceCommandHandler.java
+++ src/main/java/crqzycat/maintena/command/MaintenanceCommandHandler.java
@@ -26,7 +26,7 @@ public class MaintenanceCommandHandler {
     ) {
         dispatcher.register(
                 Commands.literal("maintenance")
-                        .requires(source -> source.hasPermission(2))
+                        .requires(source -> source.hasPermissionLevel(2))
 
                         .then(Commands.literal("on")
                                 .then(Commands.argument(
